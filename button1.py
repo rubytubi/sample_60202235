4c9b7d2c5b8317580a9d56af951f64fe281b4fbc
SeongminLee <seongmin001015@naver.com>
2023-09-11 14:32:25 +0900

    print name and hakbun

---
+++ button1.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+window =Tk()
+
+b1 = Button(window, text="Lee seong-min")
+b2 = Button(window, text="60202235")
+b1.grid(row=0, column=0)
+b2.grid(row=1, column=1)
+
+window.mainloop()
+
